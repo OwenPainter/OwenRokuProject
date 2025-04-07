c24853dcfe6860792c35a2bdf31e61603e618616
OwenPainter <owennpainter@gmail.com>
2025-04-07 09:32:38 -0400

    first commit

---
+++ GridScreen/maker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ZIP_FILE="Gridscreen.zip"
+ROKU_IP="192.168.34.113"
+
+# Remove the existing zip file if it exists but ignore hidden zip files
+test -f "$ZIP_FILE" && [[ "$ZIP_FILE" != .* ]] && rm "$ZIP_FILE"
+
+# Compress the roku folders and stuff
+zip -r "$ZIP_FILE" components images assets source Makefile manifest
+
+echo "Compression complete: $ZIP_FILE"
+
+echo -n "Open Roku Debugger? (y/n): "
+read answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+  echo "Launching Debugger..."
+  telnet "$ROKU_IP" 8085
+elif [[ "$answer" == "n" || "$answer" == "N" ]]; then
+  echo "Cancelling..."
+else
+  echo "Invalid input. Please enter y or n."
+fi
